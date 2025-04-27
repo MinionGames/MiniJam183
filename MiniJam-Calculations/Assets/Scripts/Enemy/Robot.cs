3ff3c9c2a35a64507ed64c9bb5cbfabefd3ff65e
FatCat947 <fatcat94721@gmail.com>
2025-04-27 18:16:05 -0400

    4/27/2025 Robot Physics And Stuff
    Added Rigidbody, Circle Collider, and script to look at player to robot.

--- MiniJam-Calculations/Assets/Scripts/Enemy/Robot.cs
+++ MiniJam-Calculations/Assets/Scripts/Enemy/Robot.cs
@@ -2,6 +2,9 @@ using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
 using Game.Logistics;
+using Unity.VisualScripting;
+using static UnityEngine.RuleTile.TilingRuleOutput;
+using UnityEngine.UIElements;
 
 namespace Game.Enemy
 {
@@ -15,12 +18,27 @@ namespace Game.Enemy
         [Header("Generic Robot Settings")]
         [SerializeField]
         private float moveSpeed;
-        
+
         [Header("Dev Settings")]
         [SerializeField]
-        private bool followPlayer{get; set;}
+        private bool followPlayer { get; set; }
 
+        private Rigidbody2D rb;
 
+        [SerializeField]
+        private GameObject playerObject;
+        private void Start()
+        {
+            rb = GetComponent<Rigidbody2D>();
+        }
+
+        private void FixedUpdate()
+        {
+            Vector2 playerPosition = new Vector2(playerObject.transform.position.x, playerObject.transform.position.y);
+            Vector2 lookDir = playerPosition - new Vector2(transform.position.x, transform.position.y);
+            float angle = Mathf.Atan2(lookDir.y, lookDir.x) * Mathf.Rad2Deg - 90f;
+            transform.eulerAngles = new Vector3(transform.rotation.x, transform.rotation.y, angle);
+        }
     }
 }
 
