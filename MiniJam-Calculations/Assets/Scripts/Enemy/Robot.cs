d9090acd5152d3fcd76b536b079036dd4596bf35
FatCat947 <fatcat94721@gmail.com>
2025-04-27 21:51:02 -0400

    4/27/2025 Commit 2: The Sequel
    Starting the robot player detection thingamabob

--- MiniJam-Calculations/Assets/Scripts/Enemy/Robot.cs
+++ MiniJam-Calculations/Assets/Scripts/Enemy/Robot.cs
@@ -2,8 +2,6 @@ using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
 using Game.Logistics;
-using Unity.VisualScripting;
-using static UnityEngine.RuleTile.TilingRuleOutput;
 using UnityEngine.UIElements;
 
 namespace Game.Enemy
@@ -18,6 +16,9 @@ namespace Game.Enemy
         [Header("Generic Robot Settings")]
         [SerializeField]
         private float moveSpeed;
+
+        [SerializeField]
+        private float FOV = 20;
 
         [Header("Dev Settings")]
         [SerializeField]
@@ -38,6 +39,18 @@ namespace Game.Enemy
             Vector2 lookDir = playerPosition - new Vector2(transform.position.x, transform.position.y);
             float angle = Mathf.Atan2(lookDir.y, lookDir.x) * Mathf.Rad2Deg - 90f;
             transform.eulerAngles = new Vector3(transform.rotation.x, transform.rotation.y, angle);
+
+            Vector2 rayLeft = new Vector2(Mathf.Sin(FOV / 2), Mathf.Cos(FOV / 2));
+
+            RaycastHit2D hit = Physics2D.Raycast(transform.position, transform.up);
+
+            // If it hits something...
+            if (hit)
+            {
+                // Calculate the distance from the surface and the "error" relative
+                // to the floating height.
+                float distance = Mathf.Abs(hit.point.y - transform.position.y);
+            }
         }
     }
 }
