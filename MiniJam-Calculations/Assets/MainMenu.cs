deeb3e4939ccd64a61db254ad1f6836012577c1f
FatCat947 <fatcat94721@gmail.com>
2025-07-08 12:21:33 -0400

    More Stuff For Main Menu
    Yeah.
    
    #T004a

--- MiniJam-Calculations/Assets/MainMenu.cs
+++ MiniJam-Calculations/Assets/MainMenu.cs
@@ -1,10 +1,37 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.Animations;
+
 
 public class MainMenu : MonoBehaviour
 {
-    public void PlayGame()
+
+    private Animator animator;
+
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    public void PlayAnimation()
+    {
+        animator.SetTrigger("RunAnimation");
+    }
+
+    public void StartGame()
     {
         SceneManager.LoadScene(1);
     } 
+
+    public void OpenSettings()
+    {
+        
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+
+    
 }
